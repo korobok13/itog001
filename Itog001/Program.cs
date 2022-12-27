c325b412e35a290896b8ed0317d7c6d2e06b30cf
korobov_o <korobok13@yandex.ru>
2022-12-27 16:37:02 +0300

    создали два массива строк

--- Itog001/Program.cs
+++ Itog001/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует
+// массив из строк, длина которых меньше либо равна 3 символам. 
+
+
+
+string[] wordsLetters = new string[5]{"world", "55", "318", "home", "book"};
+string[] newArray = new string[wordsLetters.Length];
+
+void findingValue(string[] wordLetters, string[] newArray)
+{
+    int count = 0;
+    for(int i = 0; i < wordsLetters.Length; i++)
+    {
+        if(wordLetters[i].Length <= 3)
+        {
+            newArray[count] = wordLetters[i];
+            count++;
+        }
+    }
+}
+
+
